419ca5b213af78d239e289961bbe09e4f066e513
@Marcin1136 <marcinwypior1990@gmail.com>
2023-01-17 21:19:40 +0100

    silnik sie kreci

---
+++ Stepper_Motor_Website.ino
@@ -0,0 +1,40 @@
+const int dirPin = 2;
+const int stepPin = 3;
+const int stepsPerRevolution = 200;
+ 
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+void loop()
+{
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
+   Serial.begin(9600);
+ 
+  // Spin motor slowly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(2000);
+  }
+  delay(1000); // Wait a second
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin, LOW);
+ 
+  // Spin motor quickly
+  for(int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+    Serial.println("nowy krok");
+  }
+  delay(1000); // Wait a second
+}
